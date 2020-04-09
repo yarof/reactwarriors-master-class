ee5a0a5e0630cba606dae47afc1e8678076aabf7
yarof <63388227+yarof@users.noreply.github.com>
2020-04-09 12:32:38 +0300

    day1
    Первый компонент

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+function MovieItem(props) {
+  console.log("props =", props);
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.vote_average}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <MovieItem title="Title new" vote_average={10.1} />
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
